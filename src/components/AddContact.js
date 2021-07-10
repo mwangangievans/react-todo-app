7052fcdb2de6d196f5a1cc58cefe649f61c0972c
MuindeMwangangi <muindemwangangi2@gmail.com>
2021-07-10 23:27:28 +0300

    first commit

---
+++ src/components/AddContact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AddContact = ()=>{
+    return (
+        <div className="ui main">
+        <h2>Add Contact</h2>
+        </div>
+    );
+};
+export  default AddContact;
---
+++ src/components/ContactCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContactCard  = ()=>{
+    return (
+        <div >
+        </div>
+    );
+};
+export  default ContactCard;
